23e5ba6312654ee0fb11e23bdf3595a37211e006
Kodehawa <david.alejandro.rubio@gmail.com>
2022-07-08 03:23:06 -0400

    Add ImmortalUpdates source

---
+++ src/rust/madara/sources/immortalupdates/src/lib.rs
@@ -0,0 +1,56 @@
+#![no_std]
+use aidoku::{
+	error::Result, prelude::*, std::String, std::Vec, Chapter, DeepLink, Filter, Listing, Manga,
+	MangaPageResult, MangaViewer, Page,
+};
+
+use madara_template::template;
+
+fn get_data() -> template::MadaraSiteData {
+	let data: template::MadaraSiteData = template::MadaraSiteData {
+		base_url: String::from("https://immortalupdates.com/"),
+		source_path: String::from("manga"),
+		alt_ajax: true,
+		viewer: |_, categories| {
+			for category in categories {
+				match category.as_str() {
+					"Manhwa" | "Manhua" => return MangaViewer::Scroll,
+					_ => continue,
+				}
+			}
+			MangaViewer::Rtl
+		},
+		..Default::default()
+	};
+	data
+}
+
+#[get_manga_list]
+fn get_manga_list(filters: Vec<Filter>, page: i32) -> Result<MangaPageResult> {
+	template::get_manga_list(filters, page, get_data())
+}
+
+#[get_manga_listing]
+fn get_manga_listing(listing: Listing, page: i32) -> Result<MangaPageResult> {
+	template::get_manga_listing(get_data(), listing, page)
+}
+
+#[get_manga_details]
+fn get_manga_details(id: String) -> Result<Manga> {
+	template::get_manga_details(id, get_data())
+}
+
+#[get_chapter_list]
+fn get_chapter_list(id: String) -> Result<Vec<Chapter>> {
+	template::get_chapter_list(id, get_data())
+}
+
+#[get_page_list]
+fn get_page_list(id: String) -> Result<Vec<Page>> {
+	template::get_page_list(id, get_data())
+}
+
+#[handle_url]
+pub fn handle_url(url: String) -> Result<DeepLink> {
+	template::handle_url(url, get_data())
+}
